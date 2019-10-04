936fdbaff400911847451d578535ef842890234d
Kevin Lee <kevin.code@kevinlee.io>
2019-10-04 21:30:01 +1000

    Add: simple code to test if httpbin works

---
+++ src/main/scala/kevinlee/testapp/MainApp.scala
@@ -0,0 +1,33 @@
+package kevinlee.testapp
+
+import java.io.{ByteArrayOutputStream, InputStream, OutputStream}
+import java.net.URL
+
+object MainApp extends App {
+
+  def transfer(bufferSize: Int, inputStream: InputStream, outputStream: OutputStream): Unit = {
+    var read = 0L
+    val buff = new Array[Byte](bufferSize)
+    var n = inputStream.read(buff)
+    while (n > 0) {
+      outputStream.write(buff, 0, n)
+      read += n
+      n = inputStream.read(buff)
+    }
+    ()
+  }
+
+  val connection = new URL("http://localhost:8000/get").openConnection()
+  val input = connection.getInputStream
+  val output = new ByteArrayOutputStream()
+  try {
+    transfer(1024, input, output)
+  } finally {
+    output.close()
+    input.close()
+  }
+
+  println(
+    new String(output.toByteArray)
+  )
+}
