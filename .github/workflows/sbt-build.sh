07f4be8ed6b1b88407dadd147b56d56aa376193e
Kevin Lee <kevin.code@kevinlee.io>
2019-10-07 03:57:44 +1100

    Set the actually current branch for coveralls

--- .github/workflows/sbt-build.sh
+++ .github/workflows/sbt-build.sh
@@ -14,6 +14,7 @@ else
   echo ""
   cd /app
   CURRENT_BRANCH_NAME="${GITHUB_REF#refs/heads/}"
+  export CI_BRANCH=$CURRENT_BRANCH_NAME
   if [[ "$CURRENT_BRANCH_NAME" == "master" || "$CURRENT_BRANCH_NAME" == "release" ]]
   then
     sbt -J-Xmx2048m "; ++ ${scala_version}!; clean; coverage; test; coverageReport; coverageAggregate"
